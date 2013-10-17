22cad1512d8f7c23858b2597ac1cf8e967f5d067
pfleiner <pfleiner@gmail.com>
2013-10-17 13:32:21 -0700

    Importing MKNetworkKit.

--- RSOAuthEngine/RSOAuthEngine.h
+++ RSOAuthEngine/RSOAuthEngine.h
@@ -23,6 +23,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#import "MKNetworkKit.h"
+
 typedef enum _RSOAuthTokenType
 {
     RSOAuthRequestToken,
